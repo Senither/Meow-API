d4780cc2f909276ee72613942c9009e3bca1cfab
Alexis (Senither) <alexis@sen-dev.com>
2020-12-01 13:32:36 +0000

    Upgrade Lumen to version 8.0

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,11 +2,17 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
+    dirname(__DIR__)
+))->bootstrap();
+
+/*
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
+*/
 
 /*
 |--------------------------------------------------------------------------
